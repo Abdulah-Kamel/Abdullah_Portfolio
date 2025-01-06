1c86fe44a248c22635bd7ba6aeaa3c406fba2779
GODZILLA <149404654+Abdulah-Kamel@users.noreply.github.com>
2025-01-06 23:59:00 +0200

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,70 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import visit from "./visit.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import php from "./tech/php.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import laravel from "./tech/laravel.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import freelance from "./company/freelance.png";
+
+import freshcart from "./freshcart.png";
+import MovieLand from "./MovieLand.png";
+import NotesApp from "./NotesApp.png";
+import nike from "./nike.png";
+import Yummy from "./Yummy.png";
+import dainels from "./dainels.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  visit,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  php,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  laravel,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  freshcart,
+  MovieLand,
+  NotesApp,
+  freelance,
+  nike,
+  Yummy,
+  dainels,
+};
