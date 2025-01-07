e97e417f05a28c184c3bcdea105476f92f24bba3
GODZILLA <149404654+Abdulah-Kamel@users.noreply.github.com>
2025-01-07 19:17:29 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
         </div>
       </div>
 
-{/*       <ComputersCanvas /> */}
+      {!isMobile && <ComputersCanvas />}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
