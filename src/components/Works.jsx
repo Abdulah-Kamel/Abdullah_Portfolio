3261bf5526c5c5a6b61285ca5756dd8ebb0dc4fb
GODZILLA <149404654+Abdulah-Kamel@users.noreply.github.com>
2025-01-07 20:19:49 +0200

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -88,6 +88,9 @@ const Works = () => {
       <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
+          initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.1 }}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcases my skills and experience through
